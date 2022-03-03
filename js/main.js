05a6e8e2aae06240c89bfb94f6ce3b995659405c
Ever <ever4ever@yandex.ru>
2022-03-03 22:17:23 +0300

    Реализовано получение и вывод фото с JSONPLACEHOLDER с помощью AJAX-запросов и промисов

---
+++ js/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const output = document.getElementById("output");
+const urlPhotos = "https://jsonplaceholder.typicode.com/photos";
+
+const getData = (url) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState !== 4) {
+        return;
+      }
+      if (request.status === 200) {
+        const response = JSON.parse(request.responseText);
+        resolve(response);
+      } else {
+        reject(request.statusText);
+      }
+    });
+    request.send();
+  });
+};
+
+const outputPhotos = (data) => {
+  const random = Math.floor(Math.random() * data.length);
+  const obj = data[random];
+  output.innerHTML = `
+    <h4>${obj.title}</h4>
+    <img src="${obj.thumbnailUrl}" alt="${obj.title}">
+  `;
+};
+
+getData(urlPhotos)
+  .then(outputPhotos)
+  .catch((err) => console.log(err));
